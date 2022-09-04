a57eb1b813c8814ee02fdf1c4c2b7fb310d93a1b
E132 <107452563+132E@users.noreply.github.com>
2022-09-04 17:13:06 -0500

    ceated classes and made migration

--- qwixs-django/qwixs/models.py
+++ qwixs-django/qwixs/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Owner(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=255)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+
+class Business(models.Model):
+    name = models.CharField(max_length=150)
+    address = models.CharField(max_length=150)
+    phone = models.CharField(max_length=11)
+    email = models.EmailField(max_length=255)
+    owner = models.ForeignKey(
+        Owner, on_delete=models.CASCADE, related_name='businesses')
+
+    def __str__(self):
+        return self.name
+
+
+class Services(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=255)
+    business = models.ForeignKey(
+        Business, on_delete=models.CASCADE, related_name='services')
+
+    def __str__(self):
+        return self.name
